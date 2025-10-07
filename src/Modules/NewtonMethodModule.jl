2c2913d2a0763f7343d64868d6741efecbb8c009
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-10-07 16:21:31 +0200

    Add the newton methods module

---
+++ src/Modules/NewtonMethodModule.jl
@@ -0,0 +1,96 @@
+module NewtonMethodModule
+        using LinearAlgebra, Metal
+        export NewtonMethod, QuasiNewtonMethod, MultiDimentionalNewtonMethod, AproximateJacobian, AproximateJacobianCentral
+        using Term.Progress
+    
+        function NewtonMethod(f :: Function, f_prime :: Function, x₀ :: Real; δ = 1e-15, ϵ = 1e-15, maxIterations = 1000)
+            for i ∈ 1:maxIterations
+                local f_prime_x₀, f_x₀ = f_prime(x₀), f(x₀)
+
+                local h = f_x₀ / f_prime_x₀
+                if abs(h / x₀) ≤ ϵ || abs(f_x₀) ≤ δ
+                    return (c = x₀,iterations = i)
+                end
+
+                x₀ -= h 
+            end
+
+            return nothing
+        end
+
+        function QuasiNewtonMethod(f :: Function, x₀ :: Real, x₁ :: Real; δ = 1e-15, ϵ = 1e-15, maxIterations = 1000)
+            local f₀, f₁ = f(x₀), f(x₁)
+            
+            for i ∈ 1:maxIterations
+                local xₖ = x₁ - (x₁ - x₀) * f₁ / (f₁ - f₀)
+                x₀, x₁ = x₁, xₖ
+                f₀, f₁ = f(x₀), f(x₁)
+
+                println("k: $i, xₖ: $xₖ, f(xₖ): $(f(xₖ))")
+                if abs(x₁ - x₀) ≤ ϵ || abs(f(x₁)) ≤ δ
+                    return (c = x₁,iterations = i)
+                end
+            end
+
+            return nothing
+            
+        end
+
+        function MultiDimentionalNewtonMethod(F :: Function, J :: Function, x₀ :: Vector{T}; δ = 1e-10, ϵ = 1e-10, maxIterations = 1000, history::Union{Vector{Float64}, Nothing} = nothing, iteration_points :: Union{Vector{Vector{T}}, Nothing} = nothing) where T <: Real
+            for i ∈ 1:maxIterations
+                local J_x₀, F_x₀ = J(x₀), F(x₀)
+                #local Δ = SolvePLU(PLUFactorization(J_x₀), F_x₀) 
+                local Δ = - J_x₀ \ F_x₀
+                local x₁ = x₀ + Δ
+
+                Δ_norm = LinearAlgebra.norm(Δ, 2)
+                F_x₁_norm = LinearAlgebra.norm(F(x₁), 2)
+                #println("Iteration $i done out of $maxIterations")
+                #println("$(Δ_norm), $(F_x₁_norm)")
+                if history !== nothing
+                    push!(history, F_x₁_norm)
+                end
+                if iteration_points !== nothing
+                    push!(iteration_points, x₁)
+                end
+
+                if Δ_norm ≤ ϵ && F_x₁_norm ≤ δ
+                    return (c = x₁,iterations = i)
+                end
+
+                x₀ = x₁
+            end
+
+            return nothing
+        end
+
+        function AproximateJacobian(F :: Function, x₀ :: Vector{T}; t = 1e-6 :: Float64) where T <: Real
+            local N = length(x₀)
+            local J = zeros(T, N, N)
+
+            Fx₀ = F(x₀)
+            for i ∈ 1:N
+                local x = copy(x₀)
+                x[i] += t
+                J[:,i] = (F(x) - Fx₀) / t
+            end
+
+            return J
+        end
+
+        function AproximateJacobianCentral(F :: Function, x₀ :: Vector{T}; t = 1e-6 :: Float64) where T <: Real
+            local N = length(x₀)
+            local J = zeros(T, N, N)
+
+            Fx₀ = F(x₀)
+            for i ∈ 1:N
+                local x_plus = copy(x₀)
+                local x_minus = copy(x₀)
+                x_plus[i] += t
+                x_minus[i] -= t
+                J[:,i] = (F(x_plus) - F(x_minus)) / (2 * t)
+            end
+
+            return J
+        end
+end
