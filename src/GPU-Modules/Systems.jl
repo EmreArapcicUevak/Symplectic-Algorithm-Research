9a81f2318766a6e1105fdcffc517be7c0bf4e82f
VedadSiljic <siljicvedad17@gmail.com>
2026-05-03 20:02:54 +0200

    Implement Modified SE1 for the GPU

--- src/GPU-Modules/Systems.jl
+++ src/GPU-Modules/Systems.jl
@@ -30,4 +30,17 @@ function SE2(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64
 
     return R
 end
+function Modified_SE1(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+
+    R = zeros(Float64, 9 * N)
+
+    ccall((:Modified_SE1, lib), Cvoid,
+        (Ptr{Float64}, Ptr{Float64}, Clong, Cdouble, Cdouble, Cdouble, Ptr{Float64}, Cdouble, Cdouble, Ptr{Float64},
+            Cdouble, Ptr{Float64}),
+        Y, R, N, α, m, k, a, t₀, T, x₀, l₀, x_d
+    )
+
+    return R
+end
 end
