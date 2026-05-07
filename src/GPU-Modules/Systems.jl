9b7686565eec832feddf90bd4c392ab0af212656
VedadSiljic <siljicvedad17@gmail.com>
2026-05-07 19:00:06 +0200

    Implement Jacobian

--- src/GPU-Modules/Systems.jl
+++ src/GPU-Modules/Systems.jl
@@ -5,21 +5,23 @@ const lib = "systems.so"
 
 export SE1
 function SE1(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
 
+    M = length(Y) ÷ (9 * N + 1)
     R = zeros(Float64, (9 * N + 1) * M)
-    
+
+
     ccall((:SE1, lib), Cvoid,
         (Ptr{Float64}, Ptr{Float64}, Clong, Cdouble, Cdouble, Cdouble, Ptr{Float64}, Cdouble, Cdouble, Ptr{Float64},
             Cdouble, Ptr{Float64}, Clong),
         Y, R, N, α, m, k, a, t₀, T, x₀, l₀, x_d, M
     )
-    
+
     return R
 end
 function SE2(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
-
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+    M = length(Y) ÷ (9 * N + 1)
     R = zeros(Float64, (9 * N + 1) * M)
 
     ccall((:SE2, lib), Cvoid,
@@ -31,8 +33,8 @@ function SE2(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64
     return R
 end
 function Modified_SE1(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
-
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+    M = length(Y) ÷ (9 * N)
     R = zeros(Float64, 9 * N * M)
 
     ccall((:Modified_SE1, lib), Cvoid,
@@ -44,8 +46,8 @@ function Modified_SE1(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k
     return R
 end
 function Modified_SE2(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
-
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+    M = length(Y) ÷ (9 * N)
     R = zeros(Float64, 9 * N * M)
 
     ccall((:Modified_SE2, lib), Cvoid,
@@ -57,8 +59,8 @@ function Modified_SE2(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k
     return R
 end
 function MidPoint(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
-
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+    M = length(Y) ÷ (9 * N + 1)
     R = zeros(Float64, (9 * N + 1) * M)
 
     ccall((:MidPoint, lib), Cvoid,
@@ -70,8 +72,8 @@ function MidPoint(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Fl
     return R
 end
 function Modified_MidPoint(Y::Vector{Float64}; N::Integer, α::Float64, m::Float64, k::Float64, a::Vector{Float64},
-    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64}, M::Integer=1)::Vector{Float64}
-
+    t₀::Float64, T::Float64, x₀::Vector{Float64}, l₀::Float64, x_d::Vector{Float64})::Vector{Float64}
+    M = length(Y) ÷ (9 * N)
     R = zeros(Float64, 9 * N * M)
 
     ccall((:Modified_MidPoint, lib), Cvoid,
