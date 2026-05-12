399bb5fcac10b2d6c19220228e8dc02800fa3df0
Emre Arapcic-Uevak <emrearapcicuevak@gmail.com>
2026-05-12 16:51:54 +0200

    Use settings.jl for Remote Status Notifies and make the code safe, in case the notification failed

--- src/Modules/Remote_Status_Notifier.jl
+++ src/Modules/Remote_Status_Notifier.jl
@@ -1,25 +1,33 @@
 module Remote_Status_Notifier
-    using HTTP, JSON3, DotEnv
-    DotEnv.load!() 
-
-    const NTFY_TOPIC = ENV["NTFY_TOPIC"]
+    using HTTP, JSON3
+    include("Settings.jl")
 
     # in Remote_Status_Notifier.jl
-    function send_message(data, url = "http://167.99.143.133:3000/data")
+    function send_message(data)
+        if isnothing(Settings.HTTP_URL)
+            @warn "HTTP_URL not set, skipping HTTP notification"
+            return
+        end
+
         try
-            HTTP.post(url, ["Content-Type" => "application/json"], JSON3.write(data))
+            HTTP.post(Settings.HTTP_URL, ["Content-Type" => "application/json"], JSON3.write(data))
         catch e
             @warn "notifier failed, continuing" exception=e
         end
     end
 
     function send_ntfy_message(msg::String; title :: String = "", tags :: String = "", priority :: String = "low")
+        if isnothing(Settings.NTFY_TOPIC)
+            @warn "NTFY_TOPIC not set, skipping ntfy notification"
+            return
+        end
+
         try
             run(`curl -s -o /tmp/ntfy.log --max-time 5 
             -H "Title: $(title)"
             -H "Tags: $(tags)"
             -H "Priority: $(priority)"
-            -d $(msg) ntfy.sh/$(NTFY_TOPIC)`)
+            -d $(msg) ntfy.sh/$(Settings.NTFY_TOPIC)`)
         catch e
             @warn "ntfy failed" exception=e
         end
