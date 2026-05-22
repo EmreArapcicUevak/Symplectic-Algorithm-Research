f1086abaf2e77568d9c42879dd956397a150f977
Emre Arapcic-Uevak <emrearapcicuevak@gmail.com>
2026-05-22 15:35:24 +0200

    Add a way to switch from using the CPU and or the GPU implementation

--- src/Modules/Settings.jl
+++ src/Modules/Settings.jl
@@ -3,23 +3,12 @@ module Settings
     import Pkg ; Pkg.activate(PROJECT_ROOT) ; Pkg.instantiate()
     using DotEnv ; DotEnv.load!() 
 
-    include("Systems.jl"); include("forward_backward_sweep.jl")
-    const name_to_func = Dict(
-        "SE1" => Systems.SE1,
-        "SE2" => Systems.SE2,
-        "Modified SE1" => Systems.Modified_SE1,
-        "Modified SE2" => Systems.Modified_SE2,
-        "MidPoint" => Systems.MidPoint,
-        "Modified MidPoint" => Systems.Modified_MidPoint,
-    )
+    include("Systems.jl"); include("forward_backward_sweep.jl"); include("../GPU-Modules/Systems.jl")
+    include("NewtonMethodModule.jl") ;include("../GPU-Modules/NewtonMethodModule.jl")
 
     const EDUCATED_GUESS_CHOICES = ["Random", "RK4ForwardBackward", "ForwardBackward"]
-    const educated_guess = Dict(
-        "RK4ForwardBackward" => forward_backward_sweep_module.RK4_forward_backward_sweep,
-        "ForwardBackward" => forward_backward_sweep_module.forward_backward_sweep,
-        "Random" => (u :: Vector{Float64}, a :: Float64, b :: Float64; y₀ :: Vector{Float64}, k_spring :: Float64 = 1.0, m :: Float64 = 1.0, x_d :: Vector{Float64}, N :: Int64 , α :: Float64, α₀ᴮᴮ :: Float64 = 1e-1, ϵ :: Float64 = 1e-6, α_max :: Float64 = 1., α_min :: Float64 = 1e-6, max_iter :: Int64 = -1, l₀ :: Float64) -> randn(Float64, 9N + 1)
-    )
 
+    const USE_GPU = parse(Bool, get(ENV, "USE_GPU", "false"))
 
     const BATCH_SAVE_SIZE = parse(Int, get(ENV, "BATCH_SAVE_SIZE", "5"))
     const NTFY_TOPIC = get(ENV, "NTFY_TOPIC", nothing)
@@ -36,4 +25,26 @@ module Settings
     mkpath(COMPUTATION_RESULTS_FOLDER)
     mkpath(FIGURE_RESULTS_FOLDER)
     mkpath(SIMULATIONS_FOLDER)
+
+
+    const name_to_func = Dict(
+        "SE1" => USE_GPU ? Systems_GPU.SE1 : Systems.SE1,
+        "SE2" => USE_GPU ? Systems_GPU.SE2 : Systems.SE2,
+        "Modified SE1" => USE_GPU ? Systems_GPU.Modified_SE1 : Systems.Modified_SE1,
+        "Modified SE2" => USE_GPU ? Systems_GPU.Modified_SE2 : Systems.Modified_SE2,
+        "MidPoint" => USE_GPU ? Systems_GPU.MidPoint : Systems.MidPoint,
+        "Modified MidPoint" => USE_GPU ? Systems_GPU.Modified_MidPoint : Systems.Modified_MidPoint,
+    )
+
+    
+    const jacobian_approximations = Dict(
+        "AproximateJacobian" => USE_GPU ? NewtonMethodModule_GPU.AproximateJacobian : NewtonMethodModule.AproximateJacobian,
+        "AproximateJacobianCentral" => USE_GPU ? NewtonMethodModule_GPU.AproximateJacobianCentral : NewtonMethodModule.AproximateJacobianCentral
+    )
+
+    const educated_guess = Dict(
+        "RK4ForwardBackward" => forward_backward_sweep_module.RK4_forward_backward_sweep,
+        "ForwardBackward" => forward_backward_sweep_module.forward_backward_sweep,
+        "Random" => (u :: Vector{Float64}, a :: Float64, b :: Float64; y₀ :: Vector{Float64}, k_spring :: Float64 = 1.0, m :: Float64 = 1.0, x_d :: Vector{Float64}, N :: Int64 , α :: Float64, α₀ᴮᴮ :: Float64 = 1e-1, ϵ :: Float64 = 1e-6, α_max :: Float64 = 1., α_min :: Float64 = 1e-6, max_iter :: Int64 = -1, l₀ :: Float64) -> randn(Float64, 9N + 1)
+    )
 end
